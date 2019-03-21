ce3a21ab88bf27ccf46bf168c449b28d1f09ffbb
Enzo Barrett <enzobarrett@gmail.com>
2019-03-20 20:07:42 -0600

    Finish regex finding of comic number, should work for all comics

--- xkcd.py
+++ xkcd.py
@@ -8,7 +8,6 @@ os.makedirs('xkcd', exist_ok=True)   # store comics in ./xkcd
 prevnum = 2124+1
 while not url.endswith('#'):
     # Download the page.
-    # print('Downloading page %s...' % url)
     res = requests.get(url)
     res.raise_for_status()
 
@@ -17,8 +16,24 @@ while not url.endswith('#'):
     # Find the URL of the comic image.
     comicElem = soup.select('#comic img')
     numberElem = soup.select('#middleContainer')
-    reElem = re.findall('\d*\d', numberElem[0].text)
-    number = reElem[len(reElem)-1]
+    # Find all numbers in text 
+    reElem = re.findall('\d+?\d*', numberElem[0].text)
+    x = 0
+
+    # Loop through found numbers until the right one is found
+    while int(prevnum)-1 != int(reElem[x]):
+        if int(reElem[x]) == 403:
+            prevnum = int(prevnum)-1
+            break
+        x+=1
+
+    # Define the number
+    number = reElem[x]
+    
+    # Check for the right number
+    if int(prevnum)-1 != int(number):
+        raise ValueError('Wrong number!')
+    prevnum = number
     
     if comicElem == []:
          print('Could not find comic image.')
@@ -26,7 +41,6 @@ while not url.endswith('#'):
          try:
              comicUrl = 'http:' + comicElem[0].get('src')
              # Download the image.
-             #print('Downloading image %s...' % (comicUrl))
              res = requests.get(comicUrl)
              res.raise_for_status()
          except requests.exceptions.MissingSchema:
@@ -35,6 +49,7 @@ while not url.endswith('#'):
              url = 'http://xkcd.com' + prevLink.get('href')
              continue
 
+    # Append the comic number and put back on extension
     extension = os.path.basename(comicUrl)[-4:]
     filename = os.path.join('xkcd', os.path.basename(comicUrl)[:-4] + '-' + number + extension)
     print(filename)
@@ -48,6 +63,5 @@ while not url.endswith('#'):
     # Get the Prev button's url.
     prevLink = soup.select('a[rel="prev"]')[0]
     url = 'http://xkcd.com' + prevLink.get('href')
-
 
 print('Done.')
