61ea40104512f603e889c05d461111b03a46b1b4
Enzo Barrett <enzobarrett@gmail.com>
2019-04-11 23:07:36 -0600

    Add database functionality
    - Loops through emails in database and sends email to each person
    - Adds the custom unsub button with their key
    - Nicely commented!

--- mail.py
+++ mail.py
@@ -1,6 +1,7 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
+import mysql.connector as mariadb
 import smtplib
 import random
 import os
@@ -8,22 +9,18 @@ import re
 
 # Pick random file
 random = random.choice(os.listdir("xkcd"))
-print(random)
 
 # Find comic number for explain link
 reElem = re.findall('\d+?\d*', random)
 number = reElem[len(reElem)-1]
-print(number)
 
-# Who to?
+# From
 From = 'enzobarrett@gmail.com'
-To = ['enzobarrett@gmail.com']
 
 # Setup message
 msg = MIMEMultipart('related')
 msg['Subject'] = 'Daily XKCD'
 msg['From'] = From
-msg['To'] = ', '.join(To)
 
 # Alternate message
 msgAlternative = MIMEMultipart('alternative')
@@ -33,8 +30,8 @@ msg.attach(msgAlternative)
 msgText = MIMEText('This is the alternative plain text message.')
 msgAlternative.attach(msgText)
 
-# Give html with image and explain link
-msgText = MIMEText('<img src="cid:image1"><br><h3><a href="explainxkcd.com/' + number + '">Explain XKCD</a></h3>', 'html')
+# Give html with image and explain link and unsub with format string
+msgText = MIMEText('<img src="cid:image1"><br><h3><a href="explainxkcd.com/' + number + '">Explain XKCD</a></h3><a href="xkcd.enzob.xyz/{key}">unsubscribe</a>', 'html')
 msgAlternative.attach(msgText)
 
 # Open image
@@ -45,10 +42,9 @@ fp.close()
 # Add proper header and name, attach image
 msgImage.add_header('Content-ID', '<image1>')
 msgImage.add_header('Content-Disposition', 'inline', filename=random)
-print('Attaching image...')
 msg.attach(msgImage)
 
-# Login
+# Start connection
 smtp = smtplib.SMTP('smtp.gmail.com', 587)
 smtp.ehlo()
 smtp.starttls()
@@ -57,8 +53,19 @@ smtp.starttls()
 with open('pass.txt', 'r') as myfile:
     password=myfile.read().replace('\n', '')
 
-# Login and send
-smtp.login('enzobarrett@gmail.com', password)
-smtp.sendmail(From, To, msg.as_string())
-smtp.quit()
+# Fetch all people to email to
+mariadb_connection = mariadb.connect(user='dailyxkcd', password='nA-C/]eB1}', database='dailyxkcd')
+cursor = mariadb_connection.cursor()
+cursor.execute("SELECT * from emails")
+result = cursor.fetchall()
 
+# Login
+smtp.login('enzobarrett@gmail.com', password)
+
+# Loop trough emails and send with key in unsub link
+for to, key in result:
+    print('sending to : ', to)
+    msg['To'] = to
+    smtp.sendmail(From, to, msg.as_string().format(key=key))
+
+smtp.quit()
